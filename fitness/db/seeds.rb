253153c4c540021a15aee8cedfe85ce44323dee3
Ubuntu <ubuntu@ip-172-31-29-137.ap-northeast-1.compute.internal>
2016-10-29 15:31:30 +0000

    seeds

--- fitness/db/seeds.rb
+++ fitness/db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -8,37 +9,42 @@
 
 require "csv"
 CSV.foreach('db/muscle.csv') do |row|
-  Muscle.create(:part => row[0], :recove_time => row[1])
+	Muscle.create(:part => row[0], :recove_time => row[1])
 
 end
 require "csv"
 CSV.foreach('db/event.csv') do |row|
-  Event.create(:event_name => row[1], :event_class => row[2])
+	Event.create(:event_name => row[1], :event_class => row[2])
 
 end
 
 require "csv"
-CSV.foreach('db/muscle_of_event.csv') do |row|
-  MuscleOfEvent.create(:event_id => row[1], :muscle_id => row[2])
+CSV.foreach('db/Muscle_of_event.csv') do |row|
+	MuscleOfEvent.create(:event_id => row[1], :muscle_id => row[2])
 
 end
 
 CSV.foreach('db/weight.csv') do |row|
-  Weight.create(:profile_id => row[1], :weight => row[2], :diary_date => row[3])
+	Weight.create(:profile_id => row[1], :weight => row[2], :diary_date => row[3])
 end
 
 CSV.foreach('db/ranking_test.csv') do |row|
-  MuscleDiary.create(:user_id => row[1], :event_id => row[2], :weight => row[3], :num => row[4], :set_num => row[5], :diary_date => row[6])
+	MuscleDiary.create(:user_id => row[1], :event_id => row[2], :weight => row[3], :num => row[4], :set_num => row[5], :diary_date => row[6])
 end
 
 CSV.foreach('db/muscle_mass.csv') do |row|
-  MuscleMass.create(:profile_id => row[1], :muscle_mass => row[2], :fat => row[3], :diary_date => row[4])
+	MuscleMass.create(:profile_id => row[1], :muscle_mass => row[2], :fat => row[3], :diary_date => row[4])
 end
 
 CSV.foreach('db/users.csv') do |row|
-  User.create(:id => row[0], :created_at => row[1], :updated_at => row[2], :email => row[3], :password => row[4])
+	User.create(:id => row[0], :email => row[1], :password => row[2])
 end
 
 CSV.foreach('db/profile.csv') do |row|
-  Profile.create(:user_id => row[1])
+	Profile.create(:id => row[0], :name => row[1], :sex => row[2], :birthday => row[3], :user_id => row[4], :image => row[5], :adress => row[6], :comment => row[7])
 end
+
+CSV.foreach('db/message.csv') do |row|
+	Message.create(:id => row[0], :user_id => row[1], :sent_userid => row[2], :message => row[3])
+end
+				  
